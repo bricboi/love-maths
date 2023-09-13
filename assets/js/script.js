ae39e7aaa439d9d8d5bbd9287fcca466d7cc8238
bricboi <>
2023-09-13 07:37:52 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,9 +50,11 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     }
     else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${CalculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(CalculatedAnswer[1]);
@@ -79,11 +81,23 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
